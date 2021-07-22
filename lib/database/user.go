16c41126318daf3797a41724b90c7407f5369d1d
Mohammad Ryan Priatama <ryanpriatama81@gmail.com>
2021-07-22 23:02:16 +0700

    add function loginUsers in lin/db

--- lib/database/user.go
+++ lib/database/user.go
@@ -1,1 +1,69 @@
 package database
+
+import (
+	"vuejs/colabs/config"
+	"vuejs/colabs/middlewares"
+	"vuejs/colabs/models"
+)
+
+func GetUsers() (interface{}, error) {
+	var users []models.User
+	if err := config.DB.Find(&users).Error; err != nil {
+		return nil, err
+	}
+	return users, nil
+}
+
+func CreateUser(users models.User) (interface{}, error) {
+
+	if err := config.DB.Save(&users).Error; err != nil {
+		return nil, err
+	}
+	return users, nil
+}
+func UpdateUser(users models.User, id int) (interface{}, error) {
+
+	if err := config.DB.Find(&users, "id=?", id).Error; err != nil {
+		return nil, err
+	}
+
+	if err := config.DB.Save(&users).Error; err != nil {
+		return nil, err
+	}
+	return users, nil
+}
+
+func DeleteUser(id int) (interface{}, error) {
+	var users []models.User
+	if err := config.DB.Find(&users, "id=?", id).Error; err != nil {
+		return nil, err
+	}
+	if err := config.DB.Delete(&users, "id=?", id).Error; err != nil {
+		return nil, err
+	}
+	return users, nil
+}
+
+func GetUser(id int) (interface{}, error) {
+	var users models.User
+
+	if err := config.DB.Find(&users, "id=?", id).Error; err != nil {
+		return nil, err
+	}
+	return users, nil
+}
+
+func LoginUsers(user *models.User) (interface{}, error) {
+	var err error
+	if err = config.DB.Where("email = ? AND password = ?", user.Email, user.Password).Error; err != nil {
+		return nil, err
+	}
+	user.Token, err = middlewares.CreateToken(int(user.ID))
+	if err != nil {
+		return nil, err
+	}
+	if err := config.DB.Save(user).Error; err != nil {
+		return nil, err
+	}
+	return user, err
+}
